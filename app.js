ac301aaeb07e0b74c1a7ebf07cb575542b7f7911
Nsuliman <nsuliman88@gmail.com>
2019-10-20 17:51:00 +0300

    build java function .. cont

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ AllProductsCont.rightObject = null;
 AllProductsCont.container = document.getElementById('products-container');
 
 AllProductsCont.leftImage = document.getElementById('left-product-image');
-AllProductsCont.rightImage = document.getElementById('middle-product-image');
+AllProductsCont.middleImage = document.getElementById('middle-product-image');
 AllProductsCont.rightImage = document.getElementById('right-product-image');
 
 
 AllProductsCont.leftTitle = document.getElementById('left-product-title');
-AllProductsCont.rightTitle = document.getElementById('middle-product-title');
-AllProductsCont.leftTitle = document.getElementById('right-product-title');
+AllProductsCont.middleTitle = document.getElementById('middle-product-title');
+AllProductsCont.rightTitle = document.getElementById('right-product-title');
 
 
 
@@ -58,9 +58,42 @@ new AllProductsCont('wine-glass', 'img/wine-glass.jpg');
 
 function renderNewProducts() {
 
+    var forbidden = [AllProductsCont.leftObject, AllProductsCont.middleObject, AllProductsCont.rightObject];
+
+    do {
+
+        AllProductsCont.leftObject = getRandomGoat();
+    
+      } while (forbidden.includes(AllProductsCont.leftObject))
+
 
 
 }  // Ending render Products function 
+
+
+AllProductsCont.leftObject.shownCtr++;                                              /// Increment The Counter 
+AllProductsCont.middleObject.shownCtr++;
+AllProductsCont.rightObject.shownCtr++;
+
+
+var leftProductImageElement = AllProductsCont.leftImage;
+var middleProductImageElement = AllProductsCont.middleImage;
+var rightProductImageElement = AllProductsCont.rightImage;
+
+
+leftProductImageElement.setAttribute('src', AllProductsCont.leftObject.src);            // Left Product 
+leftProductImageElement.setAttribute('alt', AllProductsCont.leftObject.name);
+
+middleProductImageElement.setAttribute('src', AllProductsCont.middleObject.src);        // Middle Product 
+middleProductImageElement.setAttribute('alt', AllProductsCont.middleObject.name);
+
+rightProductImageElement.setAttribute('src', AllProductsCont.rightObject.src);          // Right Product 
+rightProductImageElement.setAttribute('alt', AllProductsCont.rightObject.name);
+
+
+AllProductsCont.leftTitle.textContent = AllProductsCont.leftObject.name;
+AllProductsCont.leftTitle.textContent = AllProductsCont.leftObject.name
+AllProductsCont.rightTitle.textContent = AllProductsCont.rightObject.name;
 
 
 function getRandomProduct() {
@@ -68,3 +101,77 @@ function getRandomProduct() {
     console.log('index', index);
     return AllProductsCont.all[index];
 }  // Ending random product function 
+
+
+
+function updateTotals() {
+
+    var tableBody = document.getElementById('productsTable');
+
+    tableBody.innerHTML = '';
+
+    for (var i = 0; i < AllProductsCont.all.length; i++) {
+        var newProduct = AllProductsCont.all[i];
+        var row = addElement('tr', tableBody);
+        addElement('td', row, newProduct.title);
+        addElement('td', row, '' + newProduct.clickCtr);
+        addElement('td', row, '' + newProduct.shownCtr);
+    }
+} // Ending Total Updates product function 
+
+
+
+
+function addElement(tag, container, text) {
+
+    var element = document.createElement(tag);
+    container.appendChild(element);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+
+} // Ending Add Element  function 
+
+
+
+function clickHandler(event) {
+
+    var clickedId = event.target.id;
+    var productChlicked;
+  
+    if(clickedId === 'left-product-image') {
+      productChlicked = AllProductsCont.leftObject;
+    } else if (clickedId === 'middle-product-image') {
+      productChlicked = AllProductsCont.middleObject;
+    } if (clickedId === 'right-product-image'){
+      productChlicked = AllProductsCont.rightObject;  
+
+    }else {
+      console.log('Um, what was clicked on???', clickedId);
+    }
+  
+    if(productChlicked) {
+      productChlicked.clickCtr++;
+      AllProductsCont.roundCtr++;
+  
+      updateTotals();
+  
+      if(AllProductsCont.roundCtr === AllProductsCont.roundLimit) {
+  
+        alert('No more clicking for you!');
+  
+        AllProductsCont.container.removeEventListener('click', clickHandler);
+
+      } else {
+  
+        renderNewProducts();
+      } // Ending last else to render the new product 
+    } // Ending if statement condition if clicked 
+  } // Ending Click Handler function 
+
+    AllProductsCont.container.addEventListener('click', clickHandler);
+
+    updateTotals();
+
+    renderNewProducts();
