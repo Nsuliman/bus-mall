382a70dab69b91aa6495155ae29dd37830ba2862
Nsuliman <nsuliman88@gmail.com>
2019-10-21 14:36:09 +0300

    Chart completed for clicked products

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 
 function AllProductsCont(name, src) {
-    this.name = name;
-    this.src = src;
-    this.clickCounter = 0;
-    this.seenCounter = 0;
-    AllProductsCont.all.push(this);
+  this.name = name;
+  this.src = src;
+  this.clickCounter = 0;
+  this.seenCounter = 0;
+  AllProductsCont.all.push(this);
 }
 
 AllProductsCont.initialCounter = 0;
@@ -55,50 +55,50 @@ new AllProductsCont('wine-glass', 'img/wine-glass.jpg');
 
 function renderNewProducts() {
 
-    var forbidden = [AllProductsCont.leftObject, AllProductsCont.middleObject, AllProductsCont.rightObject];
+  var forbidden = [AllProductsCont.leftObject, AllProductsCont.middleObject, AllProductsCont.rightObject];
 
-    do {
+  do {
 
-        AllProductsCont.leftObject = getRandomProduct();
-    
-      } while (forbidden.includes(AllProductsCont.leftObject))
-      forbidden.push(AllProductsCont.leftObject);
-      do {
+    AllProductsCont.leftObject = getRandomProduct();
 
-        AllProductsCont.middleObject = getRandomProduct();
+  } while (forbidden.includes(AllProductsCont.leftObject))
+  forbidden.push(AllProductsCont.leftObject);
+  do {
 
-    } while(forbidden.includes(AllProductsCont.middleObject));
-    forbidden.push(AllProductsCont.middleObject);
-    do {
+    AllProductsCont.middleObject = getRandomProduct();
 
-        AllProductsCont.rightObject = getRandomProduct();
-    } while(forbidden.includes(AllProductsCont.rightObject));
-    // forbidden.push(AllProductsCont.rightObject);
+  } while (forbidden.includes(AllProductsCont.middleObject));
+  forbidden.push(AllProductsCont.middleObject);
+  do {
 
-    AllProductsCont.leftObject.seenCounter++;                                        /// Increment The Counter 
-//console.log('showctr' , seenCounter);   
-AllProductsCont.middleObject.seenCounter++;
-AllProductsCont.rightObject.seenCounter++;
+    AllProductsCont.rightObject = getRandomProduct();
+  } while (forbidden.includes(AllProductsCont.rightObject));
+  // forbidden.push(AllProductsCont.rightObject);
 
-
-var leftProductImageElement = AllProductsCont.leftImage;
-var middleProductImageElement = AllProductsCont.middleImage;
-var rightProductImageElement = AllProductsCont.rightImage;
+  AllProductsCont.leftObject.seenCounter++;                                        /// Increment The Counter 
+  //console.log('showctr' , seenCounter);   
+  AllProductsCont.middleObject.seenCounter++;
+  AllProductsCont.rightObject.seenCounter++;
 
 
-leftProductImageElement.setAttribute('src', AllProductsCont.leftObject.src);            // Left Product 
-leftProductImageElement.setAttribute('alt', AllProductsCont.leftObject.name);
-
-middleProductImageElement.setAttribute('src', AllProductsCont.middleObject.src);        // Middle Product 
-middleProductImageElement.setAttribute('alt', AllProductsCont.middleObject.name);
-
-rightProductImageElement.setAttribute('src', AllProductsCont.rightObject.src);          // Right Product 
-rightProductImageElement.setAttribute('alt', AllProductsCont.rightObject.name);
+  var leftProductImageElement = AllProductsCont.leftImage;
+  var middleProductImageElement = AllProductsCont.middleImage;
+  var rightProductImageElement = AllProductsCont.rightImage;
 
 
-AllProductsCont.leftTitle.textContent = AllProductsCont.leftObject.name;
-AllProductsCont.middleTitle.textContent = AllProductsCont.middleObject.name
-AllProductsCont.rightTitle.textContent = AllProductsCont.rightObject.name;
+  leftProductImageElement.setAttribute('src', AllProductsCont.leftObject.src);            // Left Product 
+  leftProductImageElement.setAttribute('alt', AllProductsCont.leftObject.name);
+
+  middleProductImageElement.setAttribute('src', AllProductsCont.middleObject.src);        // Middle Product 
+  middleProductImageElement.setAttribute('alt', AllProductsCont.middleObject.name);
+
+  rightProductImageElement.setAttribute('src', AllProductsCont.rightObject.src);          // Right Product 
+  rightProductImageElement.setAttribute('alt', AllProductsCont.rightObject.name);
+
+
+  AllProductsCont.leftTitle.textContent = AllProductsCont.leftObject.name;
+  AllProductsCont.middleTitle.textContent = AllProductsCont.middleObject.name
+  AllProductsCont.rightTitle.textContent = AllProductsCont.rightObject.name;
 
 }  // Ending render Products function 
 
@@ -107,36 +107,36 @@ AllProductsCont.rightTitle.textContent = AllProductsCont.rightObject.name;
 
 
 function getRandomProduct() {
-    var index = Math.floor(Math.random() * AllProductsCont.all.length);
-    console.log('index', index);
-    return AllProductsCont.all[index];
+  var index = Math.floor(Math.random() * AllProductsCont.all.length);
+  console.log('index', index);
+  return AllProductsCont.all[index];
 }  // Ending random product function 
 
 
 
 function updateTotals() {
 
-    var alloutput = document.getElementById('productsTable');
+  var alloutput = document.getElementById('productsTable');
 
-    alloutput.innerHTML = '';
+  alloutput.innerHTML = '';
 
-    for (var i = 0; i < AllProductsCont.all.length; i++) {
-        var newProduct = AllProductsCont.all[i];
-        var output = addElement('output',alloutput);
-        addElement('p',output, newProduct.name + ' had ' + newProduct.clickCounter + ' votes and was shown ' + newProduct.seenCounter + ' times');
-    }
+  for (var i = 0; i < AllProductsCont.all.length; i++) {
+    var newProduct = AllProductsCont.all[i];
+    var output = addElement('output', alloutput);
+    addElement('p', output, newProduct.name + ' had ' + newProduct.clickCounter + ' votes and was shown ' + newProduct.seenCounter + ' times');
+  }
 } // Ending Total Updates product function 
 
 
 
 function addElement(tag, container, text) {
 
-    var element = document.createElement(tag);
-    container.appendChild(element);
-    if (text) {
-        element.textContent = text;
-    }
-    return element;
+  var element = document.createElement(tag);
+  container.appendChild(element);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
 
 } // Ending Add Element  function 
 
@@ -144,42 +144,116 @@ function addElement(tag, container, text) {
 
 function clickHandler(event) {
 
-    var clickItemId = event.target.id;
-    var productChlicked;
-  
-    if(clickItemId === 'left-product-image') {
-      productChlicked = AllProductsCont.leftObject;
-    } else if (clickItemId === 'middle-product-image') {
-      productChlicked = AllProductsCont.middleObject;
-    } else if (clickItemId === 'right-product-image'){
-      productChlicked = AllProductsCont.rightObject;  
+  var clickItemId = event.target.id;
+  var productChlicked;
 
-    }else {
-      console.log('oooops , Finished ', clickItemId);
-    }
-  
-    if(productChlicked) {
-      productChlicked.clickCounter++;
-      AllProductsCont.initialCounter++;
-  
-      updateTotals();
-  
-      if(AllProductsCont.initialCounter === AllProductsCont.maxLimit) {
-  
-        alert('Your Trails Over ');
-  
-        AllProductsCont.container.removeEventListener('click', clickHandler);
+  if (clickItemId === 'left-product-image') {
+    productChlicked = AllProductsCont.leftObject;
+  } else if (clickItemId === 'middle-product-image') {
+    productChlicked = AllProductsCont.middleObject;
+  } else if (clickItemId === 'right-product-image') {
+    productChlicked = AllProductsCont.rightObject;
 
-      } else {
-  
-        renderNewProducts();
-      } // Ending last else to render the new product 
-    } // Ending if statement condition if clicked 
-  } // Ending Click Handler function 
+  } else {
+    console.log('oooops , Finished ', clickItemId);
+  }
 
-    AllProductsCont.container.addEventListener('click', clickHandler);
+  if (productChlicked) {
+    productChlicked.clickCounter++;
+    AllProductsCont.initialCounter++;
 
     updateTotals();
 
-    renderNewProducts();
+    if (AllProductsCont.initialCounter === AllProductsCont.maxLimit) {
 
+      alert('Your Trails Over ');
+      fullChart();
+
+      AllProductsCont.container.removeEventListener('click', clickHandler);
+
+    } else {
+
+      renderNewProducts();
+    } // Ending last else to render the new product 
+  } // Ending if statement condition if clicked 
+} // Ending Click Handler function 
+
+AllProductsCont.container.addEventListener('click', clickHandler);
+
+updateTotals();
+
+renderNewProducts();
+
+////////////////////////// Canvas ////////////////////////////////
+
+var ctx = document.getElementById('busChart').getContext('2d');
+
+
+var productsNames = [];
+var productsSeen = [];
+var productsClicks = [];
+
+
+function makeProductname() {
+
+  for (var i = 0; i < AllProductsCont.all.length; i++) {         // Store the products name in array to use it in the chart 
+    var productName = AllProductsCont.all[i].name;
+    productsNames.push(productName);
+    console.log('productsnames', productsNames);
+  }
+
+  return productsNames;
+
+}/// Ending product name function 
+
+function makeProductseen() {
+
+  for (var i = 0; i < AllProductsCont.all.length; i++) {
+    var productSeen = AllProductsCont.all[i];
+    //console.log('productClick',productClick);
+    productsSeen.push(productSeen.seenCounter);
+    console.log('productsSeen', productsSeen);
+  }
+
+  return productsSeen;
+} /// Ending product seen function 
+
+
+//////////////////////////////////////////////
+function makeProductclick() {
+
+  for (var i = 0; i < AllProductsCont.all.length; i++) {
+    var productClick = AllProductsCont.all[i];
+    productsClicks.push(productClick.clickCounter);
+    console.log('productsClicks', productsClicks);
+  }
+
+  return productsClicks;
+  
+}/// Ending product click function 
+
+
+function fullChart()
+{
+
+  console.log('Chart : ', Chart);
+  var chart = new Chart(ctx, {
+    type: 'line',
+  
+    data: {
+      labels: makeProductname(),
+      datasets: [
+        {
+          label: 'My First Dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: makeProductclick()
+        }
+      ]
+  
+    },
+    // Configuration options go here
+    options: {}
+  });
+
+}
